344c51a9c4f18641e4fccf57bebb1fa3ef09a732
Tommy Nguyen <sixty22tommy@gmail.com>
2022-06-28 15:23:54 -0700

    commit tree 4db1c75ecd3c62c1c739c87563e3bd696cc5b005

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function testingScope(){
+    
+    const localVar = 'sting of local'
+    function localFunction(){
+        const moreLocalVar = 'more local'
+        console.log(localVar)
+    }
+    // console.log(moreLocalVar)
+    console.log(customerName)
+}
+testingScope()
+// console.log(localVar)
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'matt'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'tommy'
+}
